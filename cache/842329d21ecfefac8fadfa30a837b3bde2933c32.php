47ce80f9392b480610c71ff5b29dd68f427d8529
aang-sherpa01 <np03cs4a240027@heraldcollege.edu.np>
2026-01-19 10:42:02 +0545

    Add files via upload

---
+++ cache/842329d21ecfefac8fadfa30a837b3bde2933c32.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?php echo $__env->yieldContent('title'); ?></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Simple Employee CRUD</h2>
+<hr>
+
+<?php echo $__env->yieldContent('content'); ?>
+
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\workshop8\app\views/layout.blade.php ENDPATH**/ ?>
---
+++ views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Simple Employee CRUD</h2>
+<hr>
+
+@yield('content')
+
+</body>
+</html>
